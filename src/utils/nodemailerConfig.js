09689bc4927cb783e43702f7eecf5c0c419fc69c
giannischiout <giannis.chiout@gmail.com>
2024-06-04 13:43:34 +0300

    nodemailer file

--- src/utils/nodemailerConfig.js
+++ src/utils/nodemailerConfig.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     host: process.env.NODEMAILER_HOST,
     port: process.env.NODEMAILER_PORT,
-    secure: false, // true for TLS, false for STARTTLS
+    secure:process.env.NODEMAILER_SECURE, // true for TLS, false for STARTTLS
     auth: {
       user:  process.env.NODEMAILER_USER,
       pass:  process.env.NODEMAILER_PASS,
